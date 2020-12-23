80ee0800d711d1e695e3c93a0a8d0bc01fd910dc
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-23 06:41:25 +0200

    Initial Commit

---
+++ ProbaSceneTree/ProbaSceneTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbaSceneTreeApp.swift
+//  ProbaSceneTree
+//
+//  Created by Ivan Dimitrov on 23.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ProbaSceneTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
